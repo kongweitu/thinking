f5bed7612f849fdca282f300e322eefdb34e4f55
kongweitu <kwt_iob@163.com>
2019-07-23 12:44:58 +0800

    dd

--- src/test/java/com/boot/thinking/time/TimeTest.java
+++ src/test/java/com/boot/thinking/time/TimeTest.java
@@ -19,7 +19,7 @@ public class TimeTest {
     @Test
     public void Mytest(){
         //string转localtime
-        String date = "2019-04-07T15:00:00";
+        String date = "2019-04-07T16:00:00";
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
         LocalDateTime localDateTime = LocalDateTime.parse(date,df);
         Date date1 = new Date();
